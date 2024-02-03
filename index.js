818185071b1dbe1bc08d5ad59f52728815a18fe3
squintal73 <sidnei_quintal@yahoo.com.br>
2024-02-02 22:20:04 -0300

    <h2>

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 const host = '0.0.0.0'
-
 app.get('/', (req, res) => res.send('<h2>Hello World!</h2>'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
